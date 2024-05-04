cd23f7df29228085b4d39fbebb7de21aa01f3ff2
Rikko <bulletbill51@yahoo.com>
2024-05-04 14:42:05 -0700

    fix: Forgot to push the route path

--- routes/vtuber.js
+++ routes/vtuber.js
@@ -6,5 +6,6 @@ const vtuber_controller = require('../controllers/vtuberController')
 
 router.get('/', vtuber_controller.vtuber_list);
 router.get('/:name', vtuber_controller.vtuber);
+router.get('/daily_vtuber', vtuber_controller.daily_vtuber);
 
 module.exports = router;
